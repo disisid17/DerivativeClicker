980a6c5ef2607351c3d1576842c12ed810b0729d
disisid17 <77601095+disisid17@users.noreply.github.com>
2023-10-10 16:43:58 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ window.onblur = function(){isActive=false;};
 
 //functions that add to variables
 function addMoney(money) {
-	player.money = Math.round((player.money + money*100)*100)/100;
+	player.money = Math.round((player.money + money)*100)/100;
 	if(money > 0){
 		player.totalMoneyEarned = Math.round((player.totalMoneyEarned + money)*100)/100;
 	}
@@ -58,7 +58,7 @@ function displayNum(num, ifMoney){
 //function that recalculates the multipliers associated with upgrades
 function calcMult(mult){
 	var index = mult - 1;
-	calcMult.factors = [5, 20, 50, 100, 200, 400, 600];
+	calcMult.factors = [50, 200, 500, 1000, 2000, 4000, 6000];
 	var totalBuildings = 0;
 	
 	for(var i = mult*5 - 5; i < mult*5; i++){
