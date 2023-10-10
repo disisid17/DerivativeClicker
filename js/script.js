ad913bae8e73d876d63a2e5cf12718efc9d2be29
disisid17 <77601095+disisid17@users.noreply.github.com>
2023-10-10 16:01:12 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ function displayNum(num, ifMoney){
 //function that recalculates the multipliers associated with upgrades
 function calcMult(mult){
 	var index = mult - 1;
-	calcMult.factors = [0.5, 2.0, 5, 10, 20, 40, 60];
+	calcMult.factors = [5, 20, 50, 100, 200, 400, 600];
 	var totalBuildings = 0;
 	
 	for(var i = mult*5 - 5; i < mult*5; i++){
